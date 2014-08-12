bcebea607284f271eece704a7d769caa5c52e33d
DXNNX <dxnnx@me.com>
2014-08-11 23:08:01 -0600

    Haz Esto!
    Programa la hora en la que deseas que se apague tu computador!

--- Haz Esto!/DX_AppDelegate.h
+++ Haz Esto!/DX_AppDelegate.h
@@ -8,8 +8,36 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface DX_AppDelegate : NSObject <NSApplicationDelegate>
+@interface DX_AppDelegate : NSObject <NSApplicationDelegate>{
+    IBOutlet NSTextField *output;
+    NSString *l11;
+    IBOutlet NSPanel            *panelInNib;
+    
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSView *about;
+@property (strong) IBOutlet NSPopUpButton *horas;
+@property (strong) IBOutlet NSPopUpButton *minutos;
+
+@property (strong,nonatomic) IBOutlet NSTextField *labe1;
+@property (strong,nonatomic) IBOutlet NSTextField *labe2;
+@property (strong,nonatomic) IBOutlet NSTextField *labe3;
+@property (strong,nonatomic) IBOutlet NSTextField *labe4;
+@property (strong,nonatomic) IBOutlet NSTextField *labe5;
+@property (strong,nonatomic) IBOutlet NSTextField *label;
+@property (strong,nonatomic) IBOutlet NSTextField *label1;
+@property (strong,nonatomic) IBOutlet NSTextField *label2;
+@property(strong,nonatomic) IBOutlet NSTextField *label3;
+- (IBAction)ready:(id)sender;
+- (IBAction)cancel:(id)sender;
+- (IBAction)sethoras:(id)sender;
+- (IBAction)setminutos:(id)sender;
+- (void)startthread;
+- (void)tiempo;
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app;
+- (IBAction)aboutapp:(id)sender;
+
+
 
 @end
--- Haz Esto!/DX_AppDelegate.m
+++ Haz Esto!/DX_AppDelegate.m
@@ -7,12 +7,73 @@
 //
 
 #import "DX_AppDelegate.h"
-
+#include <time.h>
 @implementation DX_AppDelegate
-
+int min=0;
+int hour=0;
+bool sig = true;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
 }
+- (void)tiempo{
+    time_t ahora = time(0);
+    struct tm *HoraActual = localtime(&ahora);
+    if (sig && HoraActual->tm_hour==hour && HoraActual->tm_min==min){
+        sig = false;
+        system("osascript -e 'tell application \"System Events\" to shut down'");
+        
+    }
+
+}
+
+- (void)startthread{
+
+    if (sig){
+        [self tiempo];
+        [NSTimer scheduledTimerWithTimeInterval:30 target:self selector:@selector(tiempo) userInfo:nil repeats:YES];
+    }
+}
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app {
+    return YES;
+}
+
+- (IBAction)ready:(id)sender {
+    sig = true;
+    NSString *mins = [NSString stringWithFormat:@"%d",min];
+    NSString *hours = [NSString stringWithFormat:@"%d",hour];
+    int diez = 10;
+    if (min<diez) {
+        mins = [NSString stringWithFormat:@"0%d",min];
+    }
+    if (hour<diez) {
+        hours = [NSString stringWithFormat:@"0%d",hour];
+    }
+    l11 = [NSString stringWithFormat:@"%@:%@", hours,mins];
+    [output setStringValue:l11];
+    [self startthread];
+}
+- (IBAction)cancel:(id)sender {
+    sig = false;
+    l11 = [NSString stringWithFormat:@"HH:MM"];
+    [output setStringValue:l11];
+}
+- (IBAction)sethoras:(id)sender {
+    NSPopUpButton *bt = (NSPopUpButton*)sender;
+    int index = [bt title].intValue;
+    hour = index;
+    
+}
+
+- (IBAction)setminutos:(id)sender {
+    NSPopUpButton *btn = (NSPopUpButton*)sender;
+    
+    int index = [btn title].intValue;
+    min = index;
+}
+- (IBAction)aboutapp:(id)sender{
+    [self->panelInNib makeKeyAndOrderFront:nil];
+}
+
+
 
 @end
